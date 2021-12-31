46ebc4409ed73698f0fcb7e53580803928525611
joecipale <72520104+jcipale@users.noreply.github.com>
2021-12-31 12:01:48 -0800

    Updated code and cleaned up output

--- acoustic.cpp
+++ acoustic.cpp
@@ -14,11 +14,19 @@
 /*--------------------------------------------------------------------------------------------*/
 
 #include <iostream>
+#include <iomanip>
+#include <fstream>
 #include <cmath>
 #include <unistd.h>
 #include <cstdlib>
+#include <string.h>
+#include <stdio.h>
+
 
 using namespace std;
+
+char nodal[4] = "-";
+char file_nodal[64];
 
 const int c =  1130;
 int height;
@@ -29,9 +37,16 @@ int length;
 int p, q, r;
 
 // Loop counter
-int i;
+int i, j, k;
 
-double frequency;
+float frequency;
+
+float axial;
+float tangent;
+float oblique;
+float sqr;
+
+float calc;
 
 // Define the list structure here
 
@@ -66,29 +81,88 @@ int main()
     cout << "Specify the room width(W):";
     cin >> width;
     
+	cout << "Save to file: ";
+	cin >> file_nodal;
+
+	strcat(file_nodal, ".txt");
+
     cout << "You have entered the following values:" << endl;
     cout << "Length :" << length << endl;
     cout << "Height :" << height << endl;
     cout << "Width  :" << width << endl;
+	cout << "Destination file: " << file_nodal << endl;
 
 	/*-----------------------------------------------------------------------*/
 	/*            Determine the frequence values for a given node            */
 	/*-----------------------------------------------------------------------*/
 
-	p = q = r = 0;
+	p = 1;
+	q = r = 0;
+	frequency = c/2;
+	calc = 0.0;
 
-	while ( frequency <= 20000 ) {
-		if ((p == 0) && (q == 0) && (r == 0)) {
-			frequency = c/2;
-			cout << " Frequency is: " << frequency  << endl;
+	ofstream outfile(file_nodal);
+	
+	outfile << "+--------------------------------------------------+" << endl;
+	outfile << "|   Freq  |  Axial | Tangent | Oblique | I | J | K |" << endl;
+	outfile << "+--------------------------------------------------+" << endl;
 
-			cout << "+-------------------------------------------------------+" << endl;
-			cout << "| Frequency | Mode Num | p  q  r |  Ax  |  Tan  |  Obl  |" << endl;
-			cout << "+-------------------------------------------------------+" << endl;
-			cout << "| " << frequency << " | " << i << " | " << " | " << p << q << r << " | " << endl;
-			cout << "+-------------------------------------------------------+" << endl;
+	for (i = 0; i <= 5; i++){
+		for (j = 0; j <= 5; j++) {
+			for (k = 0; k <= 5; k++){
+
+
+				if ( i == 0 ) {
+					axial = 000.000;
+				} else {
+					axial = (pow(i, 2)/pow(length, 2));
+				}
+
+				if ( j == 0 ) {
+					tangent = 000.000;
+				} else {
+					tangent = (pow(j, 2)/pow(width, 2));
+				}
+
+				if ( k == 0 ) {
+					 oblique = 000.000;
+				} else {
+					oblique = (pow(k, 2)/pow(height, 2));
+				}
+
+				if ((i == 0) && (j == 0) && (k ==0 )) {
+					axial = (pow(1, 2)/pow(length, 2));
+				}
+
+				sqr = sqrt(axial + tangent + oblique);
+
+				calc = frequency * sqr;
+
+				/*cout << "| " << calc << " | " << axial << " | " << tangent << " | " << oblique << " | " << i << " | " << j << " | " << k <<  " | " << endl;
+				cout << "|   ";
+				printf("%4.2f", calc);
+				cout << " | ";
+				printf("%5.4f", axial);
+				cout << "  | ";
+				printf("%5.4f", tangent);
+				cout << " | ";
+				printf("%5.4f", oblique);
+				cout << " | ";
+				printf("%d", i);
+				cout << "   | ";
+				printf("%d", j);
+				cout << "   | ";
+				printf("%d", k);
+				cout << "   | " << endl;
+				*/
+				outfile << "| " << fixed << setprecision(3) << calc << " | " << fixed << setprecision(5) << axial << " | " << fixed << setprecision(5) << tangent << " | " << fixed << setprecision(4) << oblique << " | " << i << " | " << j << " | " << k <<  " | " << endl;
+
+			}
 		}
 	}
+	outfile << "+--------------------------------------------------+" << endl;
+
+	outfile.close();
 
 
     return 0;
